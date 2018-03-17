170d7b2be7c936ed00cbab4944e431bccce31235
ManjulaNaga <knmanju488@gmail.com>
2018-03-16 21:20:36 -0700

    Another change

---
+++ src/chatroom/Authentication.java
@@ -0,0 +1,81 @@
+package chatroom;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.IvParameterSpec;
+
+import chatroom.Key;
+import java.security.PublicKey;
+import java.security.PrivateKey;
+
+public class Authentication {
+	//public static final byte[] AES_IV = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	ECKeyExchange eckey;
+	Key k;
+	Constants con;
+	public Authentication() throws Exception{ 	
+		 eckey = new ECKeyExchange();
+		//k = new Key();
+		 con = new Constants();
+	}	
+	  
+	String encryption(String originalText) throws Exception{
+		//System.out.println(" Encrypting msg..");
+	//	"ecdh-secp256r1+x509+aes128/gcm128"
+		//Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+		//use different cipher suites
+		//SecretKey skey = eckey.getSecretKey();
+		byte[] nonceBytes = Base64.getDecoder().decode(Constants.nouncestr);
+		GCMParameterSpec spec = new GCMParameterSpec(Constants.GOM_TAG_LENGTH*8, nonceBytes);
+		SecretKey skey = eckey.getSecretKey();
+		//SecretKey skey = eckey.generateSharedKey(privKey, pubKey)
+		//IvParameterSpec ivspec = new IvParameterSpec(AES_IV);
+		SecureRandom r = new SecureRandom(); // should be the best PRNG;  
+		byte[] iv = new byte[16];
+		r.nextBytes(iv);
+		// cipher.init(Cipher.ENCRYPT_MODE, skey, new IvParameterSpec(iv));
+		cipher.init(Cipher.ENCRYPT_MODE, skey, spec);		
+		System.out.println("Encrypt the data...");
+		byte[] plainText  = originalText.getBytes();
+		long startTime = System.currentTimeMillis();
+		long endTime = 0;    
+		byte[] cipherText = cipher.doFinal(plainText);
+		byte[] encodedCipherText = Base64.getEncoder().encode(cipherText);
+		String encStr = new String(encodedCipherText);
+		System.out.println("Encrypted sucessfully:" + encStr);
+		return encStr;
+    }
+	String decryption(byte[] encodedCipherText)throws Exception{
+		//System.out.println(" Decrypting msg.....");
+	
+		byte[] cipherText = Base64.getDecoder().decode(encodedCipherText);
+		SecretKey skey = eckey.getSecretKey();
+		
+		//IvParameterSpec ivspec = new IvParameterSpec(AES_IV);
+
+		// Cipher cipherD = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		Cipher cipherD = Cipher.getInstance("AES/GCM/NoPadding");
+		byte[] nonceBytes = Base64.getDecoder().decode(Constants.nouncestr);
+		GCMParameterSpec spec = new GCMParameterSpec(Constants.GOM_TAG_LENGTH*8, nonceBytes);
+		cipherD.init(Cipher.DECRYPT_MODE, skey, spec);
+		byte[] stringBytes = cipherD.doFinal(cipherText);
+		System.out.println("Decrypted: "+new String(stringBytes));
+		String str = new String(stringBytes);
+		System.out.println("Decrypted: "+str);
+		return str;
+	}
+
+	public ECKeyExchange getEckey() {
+		return eckey;
+	}
+
+	public void setEckey(ECKeyExchange eckey) {
+		this.eckey = eckey;
+	}
+
+}
---
+++ src/chatroom/Constants.java
@@ -0,0 +1,18 @@
+package chatroom;
+
+public final class Constants {
+	final static String nouncestr = "AAAAAAAAAAAAAAAA";
+	final static int GOM_TAG_LENGTH = 16;
+	final static byte[] iv = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+	final static String host = "localhost";
+	final static int port1 = 6000;
+	final static int port2 = 6010;
+	//final static String Algorithm = "AES";
+	final static String Algoinstance = "AES/GCM/NoPadding";
+	final static String cipher1 ="ecdh-secp224r1+nocert+aes128/cbc";
+	final static String cipher2 ="ecdh-secp256r1+x509+aes128/gcm128";
+
+			
+			
+		
+}
